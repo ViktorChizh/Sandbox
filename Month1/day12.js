37cc2bce2e46b1e9466737ca5ac31caf1451d6d6
ViktorChizh <130642116+ViktorChizh@users.noreply.github.com>
2023-09-19 14:25:16 +0300

    day12

---
+++ Month1/day12.js
@@ -0,0 +1,33 @@
+// ❓DESCRIPTION:
+// Make a program that filters a list of strings and returns a list with only your friends name in it.
+// If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+// Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+// ✅ SOLUTION
+function friend(friends){
+    return friends.filter(e => e.length === 4 && isNaN(e)) // почему-то сначала в массивах были только имена (судя по старым ответам), а потом добавили туда и числа (пришлось добавить проверку)
+  }
+// 🅾️ other solutions that I liked
+
+// ❓DESCRIPTION:
+
+// ✅ SOLUTION
+
+// 🅾️ other solutions that I liked
+
+// ❓DESCRIPTION:
+
+// ✅ SOLUTION
+
+// 🅾️ other solutions that I liked
+
+// ❓DESCRIPTION:
+
+// ✅ SOLUTION
+
+// 🅾️ other solutions that I liked
+
+// ❓DESCRIPTION:
+
+// ✅ SOLUTION
+
+// 🅾️ other solutions that I liked
